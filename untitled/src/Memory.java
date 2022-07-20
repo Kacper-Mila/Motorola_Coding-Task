ffa9309767a2095ab96ddfce1f43ab5e6b978d1f
Kacper-Mila <kacper.mila@gmail.com>
2022-07-21 01:23:31 +0200

    Initial commit

--- untitled/src/Memory.java
+++ untitled/src/Memory.java
@@ -2,20 +2,41 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Memory {
+    public static String [][] board = new String[2][4];
+    public static String [][] cards = new String [2][4];
+    public static Scanner scanner = new Scanner(System.in);
 
-    //Global variables
-    byte chance = 0;
-    byte numberOfWords = 0;
-    int tryes = 0;
-    String gamemode = "";
-    String name = "";
+    public static void renderBoard(){
+        for(int i = 0; i<4; i++){
+            System.out.print("|");
+            for(int j = 0; j<2; j++){
+                System.out.print(board[i][j]);
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+    }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void shuffle(){
+        Random random = new Random();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("A");
+        words.add("B");
+        words.add("C");
+        words.add("D");
+        words.add("A");
+        words.add("B");
+        words.add("C");
+        words.add("D");
 
-        
+        int index;
+        for(){}
 
     }
+
 }
