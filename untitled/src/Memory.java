af80fecfde20663fb7118d943814dc9727b79e74
Kacper-Mila <kacper.mila@gmail.com>
2022-07-21 00:40:37 +0200

    lnitial commit

--- untitled/src/Memory.java
+++ untitled/src/Memory.java
@@ -1,2 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Memory {
+
+    //Global variables
+    byte chance = 0;
+    byte numberOfWords = 0;
+    int tryes = 0;
+    String gamemode = "";
+    String name = "";
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        
+
+    }
 }
