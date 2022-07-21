5f446520f47f30a5686961f1e06fc507d6217eaf
Kacper-Mila <kacper.mila@gmail.com>
2022-07-21 13:21:20 +0200

    WIP: main concept working

--- untitled/src/Memory.java
+++ untitled/src/Memory.java
@@ -7,14 +7,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Memory {
-    public static String [][] board = new String[2][4];
-    public static String [][] cards = new String [2][4];
+    public static String [][] board = new String[4][4];
+    public static String [][] cards = new String [4][4];
     public static Scanner scanner = new Scanner(System.in);
 
     public static void renderBoard(){
-        for(int i = 0; i<4; i++){
+        for(int i = 0; i < 4; i++){
             System.out.print("|");
-            for(int j = 0; j<2; j++){
+            for(int j = 0; j < 4; j++){
                 System.out.print(board[i][j]);
                 System.out.print("|");
             }
@@ -29,14 +29,118 @@ public class Memory {
         words.add("B");
         words.add("C");
         words.add("D");
+        words.add("E");
+        words.add("F");
+        words.add("G");
+        words.add("H");
         words.add("A");
         words.add("B");
         words.add("C");
         words.add("D");
+        words.add("E");
+        words.add("F");
+        words.add("G");
+        words.add("H");
 
         int index;
-        for(){}
-
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                index = random.nextInt(words.size());
+                cards[i][j] = words.get(index);
+                words.remove(index);
+            }
+        }
     }
 
+    public static void checkInput(String [][] cards){
+        while(true){
+            if(!gameOver()){
+                System.out.println("Row: (1-4)");
+                int row1 = scanner.nextInt();
+                System.out.println("Column: (1-2)");
+                int column1 = scanner.nextInt();
+
+                if(!board[row1-1][column1-1].equals(" _ ")){
+                    System.out.println("Already entered!!");
+                    System.out.println();
+
+                    renderBoard();
+                    continue;
+                }else{
+                    board[row1-1][column1-1] = " " + cards[row1-1][column1-1] + " ";
+                    renderBoard();
+                }
+
+
+                System.out.println("Row: (1-4)");
+                int row2 = scanner.nextInt();
+                System.out.println("Column: (1-2)");
+                int column2 = scanner.nextInt();
+
+                if(!board[row2-1][column2-1].equals(" _ ")){
+                    System.out.println("Already entered!!");
+                    board[row1-1][column1-1] = " - ";
+                    System.out.println();
+
+                    renderBoard();
+                    continue;
+                }else{
+                    board[row2-1][column2-1] = " " + cards[row2-1][column2-1] + " ";
+
+                    if(board[row1-1][column1-1].equals(board[row2-1][column2-1])){
+                        renderBoard();
+                        System.out.println("Correct!");
+                    }else{
+                        renderBoard();
+                        System.out.println("Wrong!");
+                        board[row1-1][column1-1] = " _ ";
+                        board[row2-1][column2-1] = " _ ";
+                        renderBoard();
+                    }
+                }
+            }else{
+                System.out.println("Game Over!!");
+                break;
+            }
+        }
+    }
+
+    public static boolean gameOver(){
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                if(board[i][j].equals(" _ ")){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("Press n for new game or q to quit...");
+            String nq = scanner.nextLine();
+            if(nq.equals("q")){
+                System.out.println("Closing app...");
+            }else if(nq.equals("n")){
+
+                shuffle();
+                for(int i = 0; i < 4; i++){
+                    for (int j = 0; j < 4; j++) {
+                        board[i][j] = " _ ";
+                    }
+                }
+
+                renderBoard();
+                checkInput(cards);
+                break;
+
+            }else{
+                System.out.println("Invalid command");
+                continue;
+            }
+
+
+        }
+    }
 }
